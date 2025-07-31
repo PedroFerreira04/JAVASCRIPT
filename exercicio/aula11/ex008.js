bebdf2ef994fa8c391971bc0c30915a2d878f465
Pedro Henrique <Pedrohenriquefe@outlook.com>
2025-07-30 22:43:17 -0300

    atualizando e aprendendo

---
+++ exercicio/aula11/ex008.js
@@ -0,0 +1,11 @@
+let vel =  70.5
+if (vel > 60) {
+
+    console.log(`Velocidade registrada de ${vel}Km/h, ACIMA DA VELOCIDADE! MULTADO!`)
+
+}
+else {
+
+    console.log(`velocidade registrada de ${vel}Km/h, Dentro dos Limites!`)
+
+}
